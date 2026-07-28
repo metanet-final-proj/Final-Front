0389722e82e4eb87cc18c2d8489d1e17bf9db255
hyojun jeong <hyojun000404@gmail.com>
2026-07-28 10:55:31 +0900

    vite 설정 파일 수정

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,14 @@ export default defineConfig({
         target: 'http://localhost:8080',
         changeOrigin: true,
       },
+      '/oauth2': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
+      '/login/oauth2': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+      },
     },
   },
 })
